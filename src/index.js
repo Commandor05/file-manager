f73ee8dc25e8b82d62e00a68597f3fb1c6acc83c
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-06-26 23:58:54 +0300

    fix: print current path after each operation

--- src/index.js
+++ src/index.js
@@ -1,52 +1,66 @@
-import { argv, stdin as input, stdout as output } from 'process';
-import readline from 'readline';
-import { getHomeDirectory, getUpDirectory, changeDirectory } from './fs/path-manager.js';
-import { getOsInfo } from './os/os.js';
-import { dispatchFileCommand } from './fs/fs-dispatcher.js';
+import { argv, stdin as input, stdout as output } from "process";
+import readline from "readline";
+import {
+  getHomeDirectory,
+  getUpDirectory,
+  changeDirectory,
+} from "./fs/path-manager.js";
+import { getOsInfo } from "./os/os.js";
+import { dispatchFileCommand } from "./fs/fs-dispatcher.js";
 const readLine = readline.createInterface({ input, output });
-let userName = 'Unknown User';
-export const INVALID_INPUT_MESSAGE = 'Invalid input';
-export const OPERATION_FAILED_MESSAGE = 'Operation failed';
-export const FILE_CREATED = 'File was created!';
-export const FILE_RENAMED = 'File was renamed';
-export const FILE_COPIED = 'File was copied';
-export const FILE_MOVED  = 'File was moved';
-export const FILE_REMOVED  = 'File was removed';
-export const FILE_COMPRESSED  = 'File was compressed';
-export const FILE_DECOMPRESSED  = 'File was decompressed';
-let currentPath = '';
-const fileOperationCommands = ['ls', 'mv', 'cat', 'add', 'rn', 'rm', 'cp', 'compress', 'decompress'];
+let userName = "Unknown User";
+export const INVALID_INPUT_MESSAGE = "Invalid input";
+export const OPERATION_FAILED_MESSAGE = "Operation failed";
+export const FILE_CREATED = "File was created!";
+export const FILE_RENAMED = "File was renamed";
+export const FILE_COPIED = "File was copied";
+export const FILE_MOVED = "File was moved";
+export const FILE_REMOVED = "File was removed";
+export const FILE_COMPRESSED = "File was compressed";
+export const FILE_DECOMPRESSED = "File was decompressed";
+let currentPath = "";
+const fileOperationCommands = [
+  "ls",
+  "mv",
+  "cat",
+  "add",
+  "rn",
+  "rm",
+  "cp",
+  "compress",
+  "decompress",
+];
 
 const sayHi = () => {
   const message = `Welcome to the File Manager, ${userName}!`;
   printMessage(message);
-}
+};
 
 const sayBye = () => {
-  const message = `Thank you for using File Manager, ${userName}, goodbye!`
+  const message = `Thank you for using File Manager, ${userName}, goodbye!`;
   printMessage(message);
-}
+};
 
 const printPath = (path) => {
   const message = `You are currently in ${path}`;
   printMessage(message);
-}
+};
 
 export const printMessage = (message) => {
   output.write(message);
   output.write("\n");
-}
+};
 
 const parseUserNameFromArgs = () => {
-    const args = argv;
-    let values = [];
-    if (args.length === 3 && /^--/.test(args[2])) {
-      values = args[2].split('=')
-    }
+  const args = argv;
+  let values = [];
+  if (args.length === 3 && /^--/.test(args[2])) {
+    values = args[2].split("=");
+  }
 
-    if (values.length == 2) {
-      userName = values[1];
-    }
+  if (values.length == 2) {
+    userName = values[1];
+  }
 };
 
 const init = () => {
@@ -54,36 +68,38 @@ const init = () => {
   sayHi();
   currentPath = getHomeDirectory();
   printPath(currentPath);
-}
+};
 
-readLine.on('line', async (input) => {
-  let inputValues = input.split(' ');
+readLine.on("line", async (input) => {
+  let inputValues = input.split(" ");
 
-  if ( inputValues[0] === '.exit') {
+  if (inputValues[0] === ".exit") {
     process.exit();
   }
 
-  try{
-    if (inputValues[0] === 'os') {
+  try {
+    if (inputValues[0] === "os") {
       getOsInfo(inputValues[1]);
-    } else if (fileOperationCommands.some(comand => comand === inputValues[0])) {
+    } else if (
+      fileOperationCommands.some((comand) => comand === inputValues[0])
+    ) {
       dispatchFileCommand(inputValues, currentPath);
-    } else if (inputValues[0] === 'up') {
-      currentPath = getUpDirectory(currentPath)
+    } else if (inputValues[0] === "up") {
+      currentPath = getUpDirectory(currentPath);
       printPath(currentPath);
-    } else if (inputValues[0] === 'cd') {
+    } else if (inputValues[0] === "cd") {
       currentPath = await changeDirectory(currentPath, inputValues[1]);
       printPath(currentPath);
     } else {
       printMessage(INVALID_INPUT_MESSAGE);
     }
-
-  } catch(e) {
+  } catch (e) {
     printMessage(OPERATION_FAILED_MESSAGE);
   }
 
+  printPath(currentPath);
 });
 
-process.on('exit', () => sayBye());
+process.on("exit", () => sayBye());
 
 init();
